7e884726c0f136e59600f532c5ea130cc0c95c40
OwadaKazuhiro <wadaoo0223@icloud.com>
2021-03-27 11:49:57 +0900

    first-commito

--- app/controllers/homepege_controller.rb
+++ app/controllers/homepege_controller.rb
@@ -1,2 +1,5 @@
 class HomepegeController < ApplicationController
+
+  def profile
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "homepege#top"
+  get "home/top"     => "homepege#top"
+  get "home/profile" => "homepege#profile"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
